c531165e375148ba94ecbc7a441c2acb6c5acd36
kndratova <katiakondratova@gmail.com>
2024-04-14 21:57:43 +0300

    Update TimerController.cs

--- LudumDare55/Assets/LudumDare55/Architecture/Scripts/Clock/TimerController.cs
+++ LudumDare55/Assets/LudumDare55/Architecture/Scripts/Clock/TimerController.cs
@@ -1,5 +1,4 @@
-using System.Threading;
-using System.Threading.Tasks;
+using System.Collections;
 using UnityEngine;
 
 namespace LudumDare55
@@ -9,40 +8,23 @@ namespace LudumDare55
     {
         [SerializeField] private TimerView timerView;
         private TimerModel _timerModel;
-        private readonly RoundEndEvent _roundEndEvent = new();
-    
-        public async void StartTimer(CreateScriptableObjectOfSetup setup)
+        
+        public IEnumerator DecrementTimeCoroutine(float time)
         {
-            _timerModel = new TimerModel(setup.SetupTime);
+            _timerModel = new TimerModel(time);
             timerView.DisplayTime(_timerModel.GetStringTime());
             
-            await DecrementTimeAsync();
-        }
-        
-        private async Task DecrementTimeAsync()
-        {
-            var semaphore = new SemaphoreSlim(1);
-
             while (_timerModel.GetFloatTime() > 0)
             {
-                await Task.Delay(10);
-                
-                if (Application.isPlaying == false) { return; }
-
-                if (Time.timeScale == 0)
-                {
-                    await semaphore.WaitAsync(); 
-                    while (Time.timeScale == 0) await Task.Delay(100);
-                    semaphore.Release();
-                }
+                yield return new WaitForSecondsRealtime(1f);
+                if (Time.timeScale == 0) { continue; }
 
                 _timerModel.DecrementTime(1);
                 timerView.DisplayTime(_timerModel.GetStringTime());
 
-                if (_timerModel.GetFloatTime() != 0) continue;
+                if (_timerModel.GetFloatTime() != 0) { continue; }
                 
-                _roundEndEvent.InvokeEvent();
-                return;
+                yield break;
             }
         }
 
