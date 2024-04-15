77903a987412411c61dfa4dd5355f36f9be15f5e
kndratova <katiakondratova@gmail.com>
2024-04-15 05:33:01 +0300

    GamePause

---
+++ LudumDare55/Assets/LudumDare55/Architecture/Scripts/GamePause.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace LudumDare55
+{
+    public class GamePause : MonoBehaviour
+    {
+        [SerializeField] private Button pauseButton;
+        [SerializeField] private GameObject pausePanel;
+        
+        private bool _isGamePaused;
+        
+        private void Start()
+        {
+            pauseButton.onClick.AddListener(SetPause);
+        }
+
+        private void SetPause()
+        {
+            _isGamePaused = !_isGamePaused;
+            Time.timeScale = _isGamePaused ? 0 : 1;
+            pausePanel.SetActive(_isGamePaused);
+        }
+
+        private void OnDestroy()
+        {
+            pauseButton.onClick.RemoveListener(SetPause);
+        }
+    }
+}
