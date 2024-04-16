065d06f150482d30c1cd7417337672785e6d2d8e
kndratova <katiakondratova@gmail.com>
2024-04-16 03:52:05 +0300

    CUSCENE TEST

--- LudumDare55/Assets/LudumDare55/Architecture/Scripts/EndCutsceneSetter.cs
+++ LudumDare55/Assets/LudumDare55/Architecture/Scripts/EndCutsceneSetter.cs
@@ -1,10 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.Video;
 
 public class EndCutsceneSetter : MonoBehaviour
 {
-    //[SerializeField] private VideoClip[] ;
-    
+    [SerializeField] private VideoClip[] cutscenes;
+    [SerializeField] private VideoPlayer videoPlayer;
+
+    private void Awake()
+    {
+        var playerScore = PlayerPrefs.GetInt("PlayerScore");
+        
+        switch (playerScore)
+        {
+            case 15:
+                videoPlayer.clip = cutscenes[0];
+                videoPlayer.Play();
+                break;
+            case 14:
+                videoPlayer.clip = cutscenes[1];
+                videoPlayer.Play();
+                break;
+            case 13:
+                videoPlayer.clip = cutscenes[2];
+                videoPlayer.Play();
+                break;
+            default:
+                throw new NotImplementedException();
+        }
+    }
 }
