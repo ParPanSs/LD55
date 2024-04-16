b72c09c34634c892baaa80f077b2232e2e13712d
kndratova <katiakondratova@gmail.com>
2024-04-16 08:49:51 +0300

    webjl

--- LudumDare55/Assets/LudumDare55/Architecture/Scripts/LoadVideo.cs
+++ LudumDare55/Assets/LudumDare55/Architecture/Scripts/LoadVideo.cs
@@ -7,7 +7,7 @@ public class LoadVideo : MonoBehaviour
     private VideoPlayer _titl;
     private void Start()
     {
-        _titl.GetComponent<VideoPlayer>();
+        _titl = GetComponent<VideoPlayer>();
         _titl.url = System.IO.Path.Combine(Application.streamingAssetsPath, url);
     }
 }
